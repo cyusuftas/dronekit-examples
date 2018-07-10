546feb207068e7dcf0ec6614944f54d6fa91e4f0
Cahit Yusuf Taş <cyusuftas@gmail.com>
2018-07-10 11:07:14 +0300

    used decorators instead of listeners and added sitl connection option

--- observe_attribute.py
+++ observe_attribute.py
@@ -1,32 +1,33 @@
 from dronekit import connect, VehicleMode
 import time
 
-connection_string = '/dev/serial0'
+#connection_string = '/dev/serial0'      #raspberry pi
+connection_string = '127.0.0.1:14552'    #simulation
 
 #connect to the vehicle
-vehicle = connect(connection_string, wait_ready = True, baud=921600)
+#vehicle = connect(connection_string, wait_ready = True, baud=921600)
+vehicle = connect(connection_string, wait_ready = True)
 
+#use decorator to observe changes in mode
+@vehicle.on_attribute('mode')
 def callback(self, attr_name, value):
     print '%s Value: %s' % (attr_name, value)
 
+#use decorator to observe changes in any parameter
+@vehicle.parameters.on_attribute('*')
 def parameter_callback(self, attr_name, value):
     print '%s Value: %s' % (attr_name, value)
     
-#vehicle.add_attribute_listener('*', callback)  
-vehicle.add_attribute_listener('mode', callback)
 mode_changed = False
-#time.sleep(3)
-
-#parameters can also be observed by adding listeners
-vehicle.parameters.add_attribute_listener('*', parameter_callback)
+vehicle.mode = VehicleMode('STABILIZE')
 
 time_start = time.time()
 while True:    
     time_now = time.time()
     if 3 < time_now - time_start < 5 and not mode_changed:
-        vehicle.mode = VehicleMode('MANUAL')
+        vehicle.mode = VehicleMode('GUIDED')
         mode_changed = True
-    if time_now - time_start > 15:
+    if time_now - time_start > 15:  #observe changes in 15 seconds then quit
         print 'exiting'
         break
     time.sleep(0.1)
